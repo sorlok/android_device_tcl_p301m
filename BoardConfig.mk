845f150d9509b7c222ee69e1c2ed12c8d082c071
Seth N. Hetu <seth.hetu@gmail.com>
2014-09-08 22:57:40 -0400

    Trivial changes; fixing some spacing in fstab and adding a comment about FSTAB versions.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,9 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := p301m
 
+#TODO: This doesn't work, but we need a way to specify. For now, manually edit build/tools/releasetools/common.py and set hte fstab version to 1.
+#PRIVATE_RECOVERY_FSTAB_VERSION := 1
+
 BOARD_KERNEL_CMDLINE := console=ttyMT0,921600n1 vmalloc=496M slub_max_order=0 lcm=1-ili9806e_wvga_dsi_vdo_zgd fps=6134 bootprof.pl_t=3969 bootprof.lk_t=2737 printk.disable_uart=1 boot_reason=2
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 4096
