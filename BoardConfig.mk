10280a52cd07ee76d4ba1c7eb28e702182961764
Seth N. Hetu <seth.hetu@gmail.com>
2014-09-09 11:20:54 -0400

    Note on userimage

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,11 @@ TARGET_BOOTLOADER_BOARD_NAME := p301m
 #TODO: This doesn't work, but we need a way to specify. For now, manually edit build/tools/releasetools/common.py and set hte fstab version to 1.
 #PRIVATE_RECOVERY_FSTAB_VERSION := 1
 
+#TODO: This doesn't do exactly what we want; I need it to include the "USE_EXT4" flag in bootable/recovery/udater/Android.mk, but 
+#      for some reason the *system* partition is using ext4 (userdata seems to build in ext4 regardless, due to fsab.)
+#      For now modify that other .mk file; later we can try upgrading to a version 2 fstab file (which fixes a few things).
+#TARGET_USERIMAGES_USE_EXT4 := true
+
 BOARD_KERNEL_CMDLINE := console=ttyMT0,921600n1 vmalloc=496M slub_max_order=0 lcm=1-ili9806e_wvga_dsi_vdo_zgd fps=6134 bootprof.pl_t=3969 bootprof.lk_t=2737 printk.disable_uart=1 boot_reason=2
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 4096
